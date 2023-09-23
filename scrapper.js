cc174a2324f3779ef6fc432d9503157a6efd4c38
Flanker-shyam <shyamp665@gmail.com>
2023-09-23 17:27:46 +0530

    modular

--- scrapper.js
+++ scrapper.js
@@ -1,15 +1,44 @@
-// Function to extract article titles from HTML content
-module.exports = async function extractArticleTitles(html) {
-    console.log("I am here");
+const https = require('https');
+
+function extractArticleTitles(html) {
     const regex = /<a\s+href="([^"]+)">\s*<h3[^>]*>([^<]+)<\/h3>/g;
     let match;
     const results = [];
-    
-    while ((match = regex.exec(html)) !== null) {
-      const title = match[2];
-      const link = "https://time.com"+match[1];
-      results.push({ title, link });
+
+    let count = 0;
+
+    while ((match = regex.exec(html)) !== null && count <= 6) {
+        const title = match[2];
+        const link = "https://time.com" + match[1];
+        results.push({ title, link });
+        count += 1;
     }
-    
+
     return results;
 }
+
+function scrapeWebsite(url, callback) {
+    https.get(url, (response) => {
+        let data = '';
+
+        // Read the response data
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // Process the data when the response ends
+        response.on('end', () => {
+            if (response.statusCode === 200) {
+                // Process the HTML content
+                const articleTitles = extractArticleTitles(data);
+                callback(null, articleTitles);
+            } else {
+                callback('Failed to retrieve web page.', null);
+            }
+        });
+    }).on('error', (error) => {
+        callback(error, null);
+    });
+}
+
+module.exports = scrapeWebsite;
