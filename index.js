6008e9baab0c94aebcf8003df67cbc44a10c9b69
Flanker-shyam <shyamp665@gmail.com>
2023-09-23 18:04:02 +0530

    final

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const scrapeWebsite = require('./scrapper.js');
+const http = require('http');
+const scrapper = require('./scrapper');
 
-// URL of the web page to scrape
-const url = 'https://time.com/';
 
-scrapeWebsite(url, (error, articleTitles) => {
-    if (error) {
-        console.error('Error:', error);
-    } else {
-        console.log(articleTitles);
-    }
+// Create an HTTP server
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/getTimeStories' && req.method === 'GET') {
+
+    const data = await scrapper.ScarpData();
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
